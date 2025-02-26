71cfe633a7609aa3fdd0eb82037e59a46b938841
zhtlyj <3372838022@qq.com>
2025-02-26 14:47:13 +0800

    2025.2.26

--- packages/nextjs/app/createNFT/page.tsx
+++ packages/nextjs/app/createNFT/page.tsx
@@ -34,7 +34,7 @@ const saveNFTToDatabase = async (nftData: NftInfo) => {
       isListed: false
     });
     
-    const response = await fetch('/api/Nft', {
+    const response = await fetch('/api/nft', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
